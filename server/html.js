84e448768b62bb59b8a4af7cf3ee26542d25d216
TheFatLazyPotato <58443698+TheFatLazyPotato@users.noreply.github.com>
2024-10-22 22:34:19 +0200

    Update html.js
    Changed html file handling

--- server/html.js
+++ server/html.js
@@ -8,22 +8,35 @@ var args = minimist(process.argv.slice(2), opts={
 	string: 'host',
 	string: 'port',
 	alias: {h: 'host', p: 'port'},
-	default: {host: 'localhost', port: '8000'}
+	default: {host: "localhost", port: "8000"}
 });
 
 const port = Number(args.port);
+	
+function addHtmlFileToMap(htmlMap, fileLoc, url)
+{
+	fs.readFile(__dirname + fileLoc)
+		.then(cont => {htmlMap.set(url, cont);})
+		.catch(err => {
+			console.error(`File error: ${err.code}: ${err.path}`);
+			process.exit(1);
+		});
+}
 
-let indexFile;
+const htmlFiles = new Map();
+	addHtmlFileToMap(htmlFiles, "/html/index.html", "/");
+	addHtmlFileToMap(htmlFiles, "/html/indexTeste.html", "/teste");
+
 
 const requestListener = function (req, res) {
   res.setHeader("Content-Type", "text/html");
   res.writeHead(200);
-  res.end(indexFile);
+  res.end(htmlFiles.get(req.url));
 };
 
 const server = http.createServer(requestListener);
 
-fs.readFile(__dirname + "/html/index.html")
+/*fs.readFile(__dirname + "/html/index.html")
   .then(contents => {
     indexFile = contents;
     server.listen(port,args.host, () => {
@@ -33,4 +46,8 @@ fs.readFile(__dirname + "/html/index.html")
   .catch(err => {
     console.error(`Could not read index.html file: ${err}`);
     process.exit(1);
-  });
+  });*/
+  
+server.listen(port, args.host, () => {
+  console.log(`Server is running on http://${args.host}:${port}`);
+});
